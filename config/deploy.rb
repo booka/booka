ee33f54d1c1ba0bfca17967173bb9c04f1e41006
dani <danigb@gmail.com>
2010-03-27 21:01:54 +0100

    rails project

---
+++ config/deploy.rb
@@ -0,0 +1,49 @@
+# SOURCES setup users: http://www.viget.com/extend/building-an-environment-from-scratch-with-capistrano-2/
+# setup deploy: http://www.capify.org/getting-started/from-the-beginning/
+
+
+require 'yaml'
+GIT = YAML.load_file("#{File.dirname(__FILE__)}/git.yml")
+
+default_run_options[:pty] = true
+set :application, "e3e"
+set :deploy_to, "/home/deploy/#{application}"
+set :user, "deploy"
+set :use_sudo, false
+
+set :scm, "git"
+set :repository,  "git@github.com:danigb/e3e.git"
+set :branch, "master"
+set :deploy_via, :remote_cache
+set :scm_verbose, true
+set :scm_passphrase, GIT['password']
+
+role :app, "toami.net"
+role :web, "toami.net"
+role :db,  "toami.net", :primary => true
+
+after "deploy:update_code", "config:copy_shared_configurations"
+after "deploy", "deploy:cleanup"
+
+# Configuration Tasks
+namespace :config do
+  desc "copy shared configurations to current"
+  task :copy_shared_configurations, :roles => [:app] do
+    %w[database.yml wecex.yml].each do |f|
+      run "ln -nsf #{shared_path}/config/#{f} #{release_path}/config/#{f}"
+    end
+  end
+end
+
+
+namespace :deploy do
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app do ; end
+  end
+end
